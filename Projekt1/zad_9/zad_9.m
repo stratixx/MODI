a9ad70e4fad2418b2c28639e66b5d2224ce76533
Konrad W <darnokusex@wp.pl>
2018-04-23 20:27:57 +0200

    zad_9 done

--- Projekt1/zad_9/zad_9.m
+++ Projekt1/zad_9/zad_9.m
@@ -1,11 +1,24 @@
+run('../utils/init.m');
 load('../dane_poczatkowe.mat');
 
-tkonc = 250;
+tkonc = 99;
 Tp=1;
 U_vector = [ -0.9 -0.5 -0.2 0.6 ];
 
-step_matrix = [ 70 -1 -0.9; 70 -1 -0.5; 70 -1 -0.2; 70 -1 0.6 ]; %[stepTime stepInitial stepFinal; ...]
+shift = 100;
+tkonc = tkonc+shift;
+stepTime = 10+shift;
 
+step_matrix = [ stepTime -1 -0.9;...
+                stepTime -1 -0.5; ...
+                stepTime -1 -0.2; ...
+                stepTime -1 0.6; ...
+                stepTime U_vector(1)-0.1 U_vector(1)+0.1;...
+                stepTime U_vector(2)-0.1 U_vector(2)+0.1;...
+                stepTime U_vector(3)-0.1 U_vector(3)+0.1;...
+                stepTime U_vector(4)-0.1 U_vector(4)+0.1;
+                ];
+                
 
 %zaladowanie dynamicznego modelu dyskretnego
 model_dnd_name = 'dyn_nielin_mod_dyskretny';
@@ -39,26 +52,25 @@ for k=1:1:length(U_vector)
        dnd_t = dnd_data.get('tout');
        dnd_u = dnd_data.get('yout')*[0;1];
        dnd_y = dnd_data.get('yout')*[1;0];
-       
-       cut = 52;
-       
-       dzd_t = dzd_t(cut:end,:) - dzd_t(cut,:);
-       dzd_u = dzd_u(cut:end,:);
-       dzd_y = dzd_y(cut:end,:);
-       dnd_t = dnd_t(cut:end,:) - dnd_t(cut,:);
-       dnd_u = dnd_u(cut:end,:);
-       dnd_y = dnd_y(cut:end,:);
+              
+       dzd_t = dzd_t(shift:end,:) - dzd_t(shift,1);
+       dzd_u = dzd_u(shift:end,:);
+       dzd_y = dzd_y(shift:end,:);
+       dnd_t = dnd_t(shift:end,:) - dnd_t(shift,1);
+       dnd_u = dnd_u(shift:end,:);
+       dnd_y = dnd_y(shift:end,:);
 
        figure(k);
        xlabel('Czas');
        ylabel('WartoúÊ');
        hold on; box on; grid on;
-       title(strcat('Odpowiedü na skok od -1 do ', num2str(step(3)), ', U = ', num2str(U)));
+       title(strcat('Odpowiedü na skok od ', num2str(step(2)), ' do ', num2str(step(3)), ', U = ', num2str(U)));
        stairs(dzd_t, dzd_y);
        stairs(dnd_t, dnd_y);
        stairs(dnd_t, dnd_u);
        legend('Model zlinearyzowany', 'Model nieliniowy', 'Sygna≥ sterujπcy');
-       print(strcat('img/', num2str((k-1)*(1+length(U_vector))+(kStep)), 'step_', num2str(kStep), '_U_',strrep(num2str(U),'.','_') ),'-dpng');
+       num = num2str((k-1)*((size(step_matrix)*[1;0]))+(kStep));
+       print(strcat('img/', num, 'step_', num2str(kStep), '_U_',strrep(num2str(U),'.','_') ),'-dpng');
        hold off;
        close all
        %break;
